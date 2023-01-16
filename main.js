98032d35f7f43a597acfc6e4152388a2bbfd259e
midedev <ibrahimayomide330@gmail.com>
2023-01-16 16:12:52 +0100

    feat: setup mobile nav

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+// mobile navigation logic
+const hamburger = document.getElementById("hamburger");
+const openMenu = document.getElementById("menu-open");
+const closeMenu = document.getElementById("menu-close");
+const mobileNavigationMenu = document.getElementById("mobile-nav");
 
+hamburger.onclick = () => {
+  openMenu.classList.toggle("hidden");
+  closeMenu.classList.toggle("hidden");
+  mobileNavigationMenu.classList.toggle("hidden");
+};
