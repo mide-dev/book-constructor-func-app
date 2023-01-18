29b3be42ab77401feb187442ff3af9e950403edf
midedev <ibrahimayomide330@gmail.com>
2023-01-18 06:52:48 +0100

    feat: add functionality to mobile menu

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const hamburger = document.getElementById("hamburger");
 const openMenu = document.getElementById("menu-open");
 const closeMenu = document.getElementById("menu-close");
 const mobileNavigationMenu = document.getElementById("mobile-nav");
+const mainContent = document.getElementById("main-content");
 
 hamburger.onclick = () => {
   openMenu.classList.toggle("hidden");
   closeMenu.classList.toggle("hidden");
   mobileNavigationMenu.classList.toggle("hidden");
+  mainContent.classList.toggle("fixed");
 };
